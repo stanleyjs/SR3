f94894c4ace115ab72c36b4ea0879dd3adcedb92
Jay Stanley <jay.s.stanley.3@gmail.com>
2019-12-19 12:57:40 -0500

    legendre approx

---
+++ matlab/src/fit_legendre.m
@@ -0,0 +1,20 @@
+function [coeffs,error] = fit_legendre(f,order,lower, upper)
+% construct the Legendre discretization of the interval [lower,upper]
+    [xs,wts] = lgwt(order,-1,1);
+    wts = flipud(wts);
+    xs = flipud(xs);
+    ts=(lower.*(1-xs)+upper.*(1+xs))./2;      
+    fs = f(ts);
+
+    [u] = legepols(xs',order);
+    v = u';
+    for i = 1:order
+        d=1;
+        d=d*(2*i-1)/2;
+        for j = 1:order
+            u(i,j)=v(j,i)*wts(j)*d;
+        end
+    end
+    coeffs = u*fs;
+    error = 10^floor(log10(abs(coeffs(end))));
+end
